c173097cf909301e66d2dc57bea920356f41889e
YervandDavtyan <davtyanyervandgmail.com>
2022-11-09 23:35:13 +0400

    My first git commit

---
+++ Lesson1/src/Hello.java
@@ -0,0 +1,35 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.print("barev\n");
+        System.out.println("lesson 1");
+        byte x = 15 + 24;
+        System.out.println(x);
+        short y = -4542;
+        System.out.println(y);
+        int a = 454564;
+        System.out.println(a);
+        long b = 45896564765L;
+        System.out.println(b);
+
+        float aa = 444.7f;
+        double bb = 45.7;
+
+        boolean v = true;
+        System.out.println(v);
+        v = false;
+        System.out.println(v);
+
+        char t = '$';
+        System.out.println(t);
+        char tt = '\u003F';
+        System.out.println(tt);
+
+        String s = "barev";
+        System.out.println(s);
+
+        String firstname = "Yervand";
+        String lastname = "Davtyan";
+
+        System.out.println(firstname + " " + lastname);
+    }
+}
